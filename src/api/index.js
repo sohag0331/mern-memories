fca2bfc801c8c5d9db71b65793dc843f6ddc26dc
sohag0331 <sohag.0315@gmail.com>
2020-11-15 20:24:22 +0600

    URL UPDATE

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://mern-memories-project.herokuapp.com/posts";
+const url = "http://mern-memories-project.herokuapp.com/posts/";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
@@ -8,4 +8,3 @@ export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-
